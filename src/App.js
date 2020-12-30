ba76d8ce39918bda31182945bcdc806e49285961
vagrant <vagrant@udemi1.local>
2020-12-30 01:17:12 +0000

    added travis deploy config to AWS 13

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Code changed again and again!
+          Hello there!
         </p>
         <a
           className="App-link"
